9e08ba43db865aa35353f38182267782b248f33f
javawithlwl <javawithlwl@gmail.com>
2024-05-30 07:29:52 +0530

    Added ui application

--- bookstore/src/main/java/com/careerit/bookstore/config/OpenApiConfig.java
+++ bookstore/src/main/java/com/careerit/bookstore/config/OpenApiConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OpenApiConfig {
@@ -23,5 +25,19 @@ public class OpenApiConfig {
                         .license(new io.swagger.v3.oas.models.info.License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org")));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*") // Add your allowed origins
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add your allowed HTTP methods
+                    .allowedHeaders("*") // Allow all headers
+                    .maxAge(3600); // Set the max age for preflight requests
+            }
+        };
     }
 }
